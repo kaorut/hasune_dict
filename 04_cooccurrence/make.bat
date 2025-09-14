8720518ca22617a602a01e4f33ca83ff18e22b80
kaoru <kaorut@mc.pikara.ne.jp>
2025-09-14 15:14:53 +0900

    Implement

--- 04_cooccurrence/make.bat
+++ 04_cooccurrence/make.bat
@@ -23,140 +23,145 @@ set WIKIPEDIA_WORD_FREQ_FILE=..\03_word_frequency\merged_wikipedia_frequencies\w
 set WIKIPEDIA_NPMI_DIR=wikipedia_npmi
 set AOZORA_WORD_FREQ_FILE=..\03_word_frequency\merged_aozora_frequencies\word.merged_frequencies
 set AOZORA_NPMI_DIR=aozora_npmi
+set SHORT_UNIT_LEXICON=..\01_dictionary_content_1\dictionary_source\unidic.expanded.lexicon
 set WIKIPEDIA_COMPOUND_WORDS_DIR=wikipedia_compound_words
 set AOZORA_COMPOUND_WORDS_DIR=aozora_compound_words
 
-rem 19min
-time /t
-echo python %COLLECT_PHRASES_PY% ^
-    %WIKIPEDIA_MORPHEMES_DIR% ^
-    %WIKIPEDIA_PHRASES_DIR%
-python %COLLECT_PHRASES_PY% ^
-    %WIKIPEDIA_MORPHEMES_DIR% ^
-    %WIKIPEDIA_PHRASES_DIR% ^
-    || exit /b 1
+rem rem 19min
+rem time /t
+rem echo python %COLLECT_PHRASES_PY% ^
+rem     %WIKIPEDIA_MORPHEMES_DIR% ^
+rem     %WIKIPEDIA_PHRASES_DIR%
+rem python %COLLECT_PHRASES_PY% ^
+rem     %WIKIPEDIA_MORPHEMES_DIR% ^
+rem     %WIKIPEDIA_PHRASES_DIR% ^
+rem     || exit /b 1
+rem 
+rem rem 1min
+rem time /t
+rem echo python %COLLECT_PHRASES_PY% ^
+rem     %AOZORA_MORPHEMES_DIR% ^
+rem     %AOZORA_PHRASES_DIR%
+rem python %COLLECT_PHRASES_PY% ^
+rem     %AOZORA_MORPHEMES_DIR% ^
+rem     %AOZORA_PHRASES_DIR% ^
+rem     || exit /b 1
+rem 
+rem rem 7min
+rem time /t
+rem rmdir /s /q %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR%
+rem mkdir %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR%
+rem echo python %MERGE_PHRASES_PY% ^
+rem     %WIKIPEDIA_PHRASES_DIR% ^
+rem     phrase. ^
+rem     %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR%
+rem python %MERGE_PHRASES_PY% ^
+rem     %WIKIPEDIA_PHRASES_DIR% ^
+rem     phrase. ^
+rem     %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR% ^
+rem     || exit /b 1
+rem rmdir /s /q %MERGED_WIKIPEDIA_PHRASES_DIR%
+rem mkdir %MERGED_WIKIPEDIA_PHRASES_DIR%
+rem echo python %MERGE_PHRASES_PY% ^
+rem     %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR% ^
+rem     phrase. ^
+rem     %MERGED_WIKIPEDIA_PHRASES_DIR%
+rem python %MERGE_PHRASES_PY% ^
+rem     %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR% ^
+rem     phrase. ^
+rem     %MERGED_WIKIPEDIA_PHRASES_DIR% ^
+rem     || exit /b 1
+rem rename ^
+rem     %MERGED_WIKIPEDIA_PHRASES_DIR%\*.%INTERMEDIATE_WIKIPEDIA_PHRASES_DIR% ^
+rem     *.merged_phrases
+rem 
+rem rem 1min
+rem time /t
+rem rmdir /s /q %INTERMEDIATE_AOZORA_PHRASES_DIR%
+rem mkdir %INTERMEDIATE_AOZORA_PHRASES_DIR%
+rem echo python %MERGE_PHRASES_PY% ^
+rem     %AOZORA_PHRASES_DIR% ^
+rem     phrase. ^
+rem     %INTERMEDIATE_AOZORA_PHRASES_DIR%
+rem python %MERGE_PHRASES_PY% ^
+rem     %AOZORA_PHRASES_DIR% ^
+rem     phrase. ^
+rem     %INTERMEDIATE_AOZORA_PHRASES_DIR% ^
+rem     || exit /b 1
+rem rmdir /s /q %MERGED_AOZORA_PHRASES_DIR%
+rem mkdir %MERGED_AOZORA_PHRASES_DIR%
+rem echo python %MERGE_PHRASES_PY% ^
+rem     %INTERMEDIATE_AOZORA_PHRASES_DIR% ^
+rem     phrase. ^
+rem     %MERGED_AOZORA_PHRASES_DIR%
+rem python %MERGE_PHRASES_PY% ^
+rem     %INTERMEDIATE_AOZORA_PHRASES_DIR% ^
+rem     phrase. ^
+rem     %MERGED_AOZORA_PHRASES_DIR% ^
+rem     || exit /b 1
+rem rename ^
+rem     %MERGED_AOZORA_PHRASES_DIR%\*.%INTERMEDIATE_AOZORA_PHRASES_DIR% ^
+rem     *.merged_phrases
+rem 
+rem rem 1min
+rem time /t
+rem rmdir /s /q %WIKIPEDIA_NPMI_DIR%
+rem mkdir %WIKIPEDIA_NPMI_DIR%
+rem echo python %CALCULATE_NPMI_PY% ^
+rem     %WIKIPEDIA_WORD_FREQ_FILE% ^
+rem     %MERGED_WIKIPEDIA_PHRASES_DIR%\phrase.merged_phrases ^
+rem     %WIKIPEDIA_NPMI_DIR%\phrase.npmi
+rem python %CALCULATE_NPMI_PY% ^
+rem     %WIKIPEDIA_WORD_FREQ_FILE% ^
+rem     %MERGED_WIKIPEDIA_PHRASES_DIR%\phrase.merged_phrases ^
+rem     %WIKIPEDIA_NPMI_DIR%\phrase.npmi ^
+rem     || exit /b 1
+rem 
+rem rem 1min
+rem time /t
+rem rmdir /s /q %AOZORA_NPMI_DIR%
+rem mkdir %AOZORA_NPMI_DIR%
+rem echo python %CALCULATE_NPMI_PY% ^
+rem     %AOZORA_WORD_FREQ_FILE% ^
+rem     %MERGED_AOZORA_PHRASES_DIR%\phrase.merged_phrases ^
+rem     %AOZORA_NPMI_DIR%\phrase.npmi
+rem python %CALCULATE_NPMI_PY% ^
+rem     %AOZORA_WORD_FREQ_FILE% ^
+rem     %MERGED_AOZORA_PHRASES_DIR%\phrase.merged_phrases ^
+rem     %AOZORA_NPMI_DIR%\phrase.npmi ^
+rem     || exit /b 1
 
-rem 1min
-time /t
-echo python %COLLECT_PHRASES_PY% ^
-    %AOZORA_MORPHEMES_DIR% ^
-    %AOZORA_PHRASES_DIR%
-python %COLLECT_PHRASES_PY% ^
-    %AOZORA_MORPHEMES_DIR% ^
-    %AOZORA_PHRASES_DIR% ^
-    || exit /b 1
-
-rem 7min
-time /t
-rmdir /s /q %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR%
-mkdir %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR%
-echo python %MERGE_PHRASES_PY% ^
-    %WIKIPEDIA_PHRASES_DIR% ^
-    phrase. ^
-    %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR%
-python %MERGE_PHRASES_PY% ^
-    %WIKIPEDIA_PHRASES_DIR% ^
-    phrase. ^
-    %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR% ^
-    || exit /b 1
-rmdir /s /q %MERGED_WIKIPEDIA_PHRASES_DIR%
-mkdir %MERGED_WIKIPEDIA_PHRASES_DIR%
-echo python %MERGE_PHRASES_PY% ^
-    %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR% ^
-    phrase. ^
-    %MERGED_WIKIPEDIA_PHRASES_DIR%
-python %MERGE_PHRASES_PY% ^
-    %INTERMEDIATE_WIKIPEDIA_PHRASES_DIR% ^
-    phrase. ^
-    %MERGED_WIKIPEDIA_PHRASES_DIR% ^
-    || exit /b 1
-rename ^
-    %MERGED_WIKIPEDIA_PHRASES_DIR%\*.%INTERMEDIATE_WIKIPEDIA_PHRASES_DIR% ^
-    *.merged_phrases
-
-rem 1min
-time /t
-rmdir /s /q %INTERMEDIATE_AOZORA_PHRASES_DIR%
-mkdir %INTERMEDIATE_AOZORA_PHRASES_DIR%
-echo python %MERGE_PHRASES_PY% ^
-    %AOZORA_PHRASES_DIR% ^
-    phrase. ^
-    %INTERMEDIATE_AOZORA_PHRASES_DIR%
-python %MERGE_PHRASES_PY% ^
-    %AOZORA_PHRASES_DIR% ^
-    phrase. ^
-    %INTERMEDIATE_AOZORA_PHRASES_DIR% ^
-    || exit /b 1
-rmdir /s /q %MERGED_AOZORA_PHRASES_DIR%
-mkdir %MERGED_AOZORA_PHRASES_DIR%
-echo python %MERGE_PHRASES_PY% ^
-    %INTERMEDIATE_AOZORA_PHRASES_DIR% ^
-    phrase. ^
-    %MERGED_AOZORA_PHRASES_DIR%
-python %MERGE_PHRASES_PY% ^
-    %INTERMEDIATE_AOZORA_PHRASES_DIR% ^
-    phrase. ^
-    %MERGED_AOZORA_PHRASES_DIR% ^
-    || exit /b 1
-rename ^
-    %MERGED_AOZORA_PHRASES_DIR%\*.%INTERMEDIATE_AOZORA_PHRASES_DIR% ^
-    *.merged_phrases
-
-rem 1min
-time /t
-rmdir /s /q %WIKIPEDIA_NPMI_DIR%
-mkdir %WIKIPEDIA_NPMI_DIR%
-echo python %CALCULATE_NPMI_PY% ^
-    %WIKIPEDIA_WORD_FREQ_FILE% ^
-    %MERGED_WIKIPEDIA_PHRASES_DIR%\phrase.merged_phrases ^
-    %WIKIPEDIA_NPMI_DIR%\phrase.npmi
-python %CALCULATE_NPMI_PY% ^
-    %WIKIPEDIA_WORD_FREQ_FILE% ^
-    %MERGED_WIKIPEDIA_PHRASES_DIR%\phrase.merged_phrases ^
-    %WIKIPEDIA_NPMI_DIR%\phrase.npmi ^
-    || exit /b 1
-
-rem 1min
-time /t
-rmdir /s /q %AOZORA_NPMI_DIR%
-mkdir %AOZORA_NPMI_DIR%
-echo python %CALCULATE_NPMI_PY% ^
-    %AOZORA_WORD_FREQ_FILE% ^
-    %MERGED_AOZORA_PHRASES_DIR%\phrase.merged_phrases ^
-    %AOZORA_NPMI_DIR%\phrase.npmi
-python %CALCULATE_NPMI_PY% ^
-    %AOZORA_WORD_FREQ_FILE% ^
-    %MERGED_AOZORA_PHRASES_DIR%\phrase.merged_phrases ^
-    %AOZORA_NPMI_DIR%\phrase.npmi ^
-    || exit /b 1
-
-rem 1min
+rem <1min
 time /t
 rmdir /s /q %WIKIPEDIA_COMPOUND_WORDS_DIR%
 mkdir %WIKIPEDIA_COMPOUND_WORDS_DIR%
 echo python %COMPOUND_WORDS_PY% ^
     %WIKIPEDIA_NPMI_DIR%\phrase.npmi ^
+    %SHORT_UNIT_LEXICON% ^
     %WIKIPEDIA_COMPOUND_WORDS_DIR%\compound_word.lexicon ^
     %WIKIPEDIA_COMPOUND_WORDS_DIR%\compound_word.frequencies ^
     %WIKIPEDIA_COMPOUND_WORDS_DIR%\rest_phrase.npmi
 python %COMPOUND_WORDS_PY% ^
     %WIKIPEDIA_NPMI_DIR%\phrase.npmi ^
+    %SHORT_UNIT_LEXICON% ^
     %WIKIPEDIA_COMPOUND_WORDS_DIR%\compound_word.lexicon ^
     %WIKIPEDIA_COMPOUND_WORDS_DIR%\compound_word.frequencies ^
     %WIKIPEDIA_COMPOUND_WORDS_DIR%\rest_phrase.npmi ^
     || exit /b 1
 
-rem 1min
+rem <1min
 time /t
 rmdir /s /q %AOZORA_COMPOUND_WORDS_DIR%
 mkdir %AOZORA_COMPOUND_WORDS_DIR%
 echo python %COMPOUND_WORDS_PY% ^
     %AOZORA_NPMI_DIR%\phrase.npmi ^
+    %SHORT_UNIT_LEXICON% ^
     %AOZORA_COMPOUND_WORDS_DIR%\compound_word.lexicon ^
     %AOZORA_COMPOUND_WORDS_DIR%\compound_word.frequencies ^
     %AOZORA_COMPOUND_WORDS_DIR%\rest_phrase.npmi
 python %COMPOUND_WORDS_PY% ^
     %AOZORA_NPMI_DIR%\phrase.npmi ^
+    %SHORT_UNIT_LEXICON% ^
     %AOZORA_COMPOUND_WORDS_DIR%\compound_word.lexicon ^
     %AOZORA_COMPOUND_WORDS_DIR%\compound_word.frequencies ^
     %AOZORA_COMPOUND_WORDS_DIR%\rest_phrase.npmi ^
