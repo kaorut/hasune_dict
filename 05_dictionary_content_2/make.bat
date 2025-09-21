ad79cd56ccde597905841b15b9992d24a1f6904b
kaoru <kaorut@mc.pikara.ne.jp>
2025-09-21 11:05:52 +0900

    Implement

--- 05_dictionary_content_2/make.bat
+++ 05_dictionary_content_2/make.bat
@@ -8,11 +8,9 @@ set MAKE_DICT_CONTENT_PY=%MAKE_DICT_CONTENT_BASE%\hasune.make_dict_content.py
 set UNIDIC_LEX_CSV=..\unidic-cwj-202302_full\lex.csv
 set OTHER_LEX_CSV_BASE=%MAKE_DICT_CONTENT_BASE%\data
 set DICT_SRC_DIR=..\01_dictionary_content_1\dictionary_source
-set WIKIPEDIA_COMPOUND_DICT_SRC_DIR=..\04_cooccurrence\wikipedia_compound_words
-set AOZORA_COMPOUND_DICT_SRC_DIR=..\04_cooccurrence\aozora_compound_words
+set COMPOUND_WORD_COMPOUND_DICT_SRC_DIR=..\04_cooccurrence\merged_compound_words
 set DICT_CONTENT_NAME=main
 set DICT_CONTENT_DIR=dictionary_content
-set RECORD_LIST=%DICT_CONTENT_DIR%\%DICT_CONTENT_NAME%.record_list
 
 rem 1min
 time /t
@@ -22,19 +20,17 @@ echo python %MAKE_DICT_CONTENT_PY% ^
     %DICT_CONTENT_DIR% ^
     %DICT_CONTENT_NAME% ^
     %DICT_SRC_DIR%\unidic.expanded.lexicon ^
+    %COMPOUND_WORD_COMPOUND_DICT_SRC_DIR%\compound_word.expanded.lexicon ^
     %DICT_SRC_DIR%\auxverb.lexicon ^
     %DICT_SRC_DIR%\particle.lexicon ^
-    %DICT_SRC_DIR%\punctuation.lexicon ^
-    %WIKIPEDIA_COMPOUND_DICT_SRC_DIR%\compound_word.lexicon ^
-    %AOZORA_COMPOUND_DICT_SRC_DIR%\compound_word.lexicon
+    %DICT_SRC_DIR%\punctuation.lexicon
 python %MAKE_DICT_CONTENT_PY% ^
     %DICT_CONTENT_DIR% ^
     %DICT_CONTENT_NAME% ^
     %DICT_SRC_DIR%\unidic.expanded.lexicon ^
+    %COMPOUND_WORD_COMPOUND_DICT_SRC_DIR%\compound_word.expanded.lexicon ^
     %DICT_SRC_DIR%\auxverb.lexicon ^
     %DICT_SRC_DIR%\particle.lexicon ^
-    %WIKIPEDIA_COMPOUND_DICT_SRC_DIR%\compound_word.lexicon ^
-    %AOZORA_COMPOUND_DICT_SRC_DIR%\compound_word.lexicon ^
     %DICT_SRC_DIR%\punctuation.lexicon || exit /b 1
 
 time /t
